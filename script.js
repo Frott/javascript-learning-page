54ce783f754f88ae27e0cdcfbe9b133d638f4ecf
Frott <67923372+Frott@users.noreply.github.com>
2020-07-09 15:52:45 +0200

    Grey Out Finished Tasks
    - Checkboxes grey out after checked
    - Row background darkens after checked

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-window.onload = clickListener;
-function clickListener(){
+window.onload = mainFunc;
+function mainFunc() {
     let todoTask = document.getElementById("todoTask");
     let todoTable = document.getElementById("table");
-    document.getElementById("testButton").addEventListener("click", function(){
-        if (todoTask.value == null){
-            alert("Cannot add an empty Task!");
-        } else {
-            let newLine = todoTable.insertRow(-1);
-            let newCell = newLine.insertCell(0);
-            let newCheckbox = document.createElement("input");
-            let newText = document.createTextNode(" " + todoTask.value + " ");
-            let newDeleteButton = document.createElement("button");
-            newDeleteButton.textContent = "Delete";
-            newCheckbox.type = "checkbox";
-            newCell.appendChild(newCheckbox);
-            newCell.appendChild(newText);
-            newCell.appendChild(newDeleteButton);
-            todoTask.value = null;
-            newDeleteButton.addEventListener("click", function(event){
-            todoTable.deleteRow(event.target.parentElement.parentElement.rowIndex);
-            });
-        }
-    });
+    addTask();
+    function addTask(){
+        document.getElementById("testButton").addEventListener("click", function(){
+            if (todoTask.value.trim() == ""){
+                alert("Cannot add an empty Task!");
+            } else {
+                let newLine = todoTable.insertRow(-1);
+                let newCell = newLine.insertCell(0);
+                let newCheckbox = document.createElement("input");
+                let newText = document.createTextNode(" " + todoTask.value + " ");
+                let newDeleteButton = document.createElement("button");
+                newDeleteButton.textContent = "Delete";
+                newCheckbox.type = "checkbox";
+                newCheckbox.className = "checkbox";
+                newCell.appendChild(newCheckbox);
+                newCell.appendChild(newText);
+                newCell.appendChild(newDeleteButton);
+                todoTask.value = null;
+                newDeleteButton.addEventListener("click", function(event){
+                    todoTable.deleteRow(event.target.parentElement.parentElement.rowIndex);
+                });
+                newCheckbox.addEventListener("click", function(event){
+                    newCheckbox.disabled = true;
+                    todoTable.rows[event.target.parentElement.parentElement.rowIndex].bgColor = "#386161";
+                });
+            }
+        });
+    }
 }
