5c559fde181b1a7825e05f6b07052accf9c31e8d
Frott <67923372+Frott@users.noreply.github.com>
2020-07-09 18:59:23 +0200

    Move completed tasks
    - Completed tasks move to the bottom of the table
    - New tasks are insterted above completed

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ function mainFunc() {
     let counter = document.getElementById("counter");
     let tasksLeft = 0;
     let tasksDone = 0;
+    let firstCompleted = -1;
     addTask();
     function addTask(){
         document.getElementById("testButton").addEventListener("click", function(){
             if (todoTask.value.trim() == ""){
                 alert("Cannot add an empty Task!");
             } else {
-                let newLine = todoTable.insertRow(-1);
+                if (todoTable.rows.length > 0){
+                    for(let i = 0; i <= todoTable.rows.length; i++){
+                        if(todoTable.rows[i].children[0].children[0].disabled){
+                            firstCompleted = i;
+                            break;
+                        }
+                    }
+                }
+                let newLine = todoTable.insertRow(firstCompleted);
                 let newCell = newLine.insertCell(0);
                 let newCheckbox = document.createElement("input");
                 let newText = document.createTextNode(" " + todoTask.value + " ");
@@ -33,6 +42,7 @@ function mainFunc() {
                     counter.rows[1].cells[1].innerHTML = tasksLeft;
                     tasksDone++;
                     counter.rows[1].cells[0].innerHTML = tasksDone;
+                    todoTable.rows[todoTable.rows.length-1].after(event.target.parentElement.parentElement);
                 });
                 newDeleteButton.addEventListener("click", function(event){
                     todoTable.deleteRow(event.target.parentElement.parentElement.rowIndex);
