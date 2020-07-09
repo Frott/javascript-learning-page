c423e5eba5167135d353ea88c27aff2aa5f65818
Frott <67923372+Frott@users.noreply.github.com>
2020-07-09 14:19:38 +0200

    Update Page Script
    - Major bugfixes
    - Added delete button
    - Added custom tasks

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 window.onload = clickListener;
 function clickListener(){
+    let todoTask = document.getElementById("todoTask");
     let todoTable = document.getElementById("table");
     document.getElementById("testButton").addEventListener("click", function(){
-        let newLine = todoTable.insertRow(-1);
-        let newCell = newLine.insertCell(0);
-        let newCheckbox = document.createElement("input");
-        let newText = document.createTextNode(" New Line ");
-        let newDeleteButton = document.createElement("button");
-        let tempRow = todoTable.rows.length;
-        newDeleteButton.textContent = "Delete";
-        newCheckbox.type = "checkbox";
-        newCell.appendChild(newCheckbox);
-        newCell.appendChild(newText);
-        newCell.appendChild(newDeleteButton);
-        newDeleteButton.addEventListener("click", function(tempRow){
-            todoTable.deleteRow(tempRow);
-        })
+        if (todoTask.value == null || todoTask.value == ""){
+            alert("Cannot add an empty Task!");
+        } else {
+            let newLine = todoTable.insertRow(-1);
+            let newCell = newLine.insertCell(0);
+            let newCheckbox = document.createElement("input");
+            let newText = document.createTextNode(" " + todoTask.value + " ");
+            let newDeleteButton = document.createElement("button");
+            newDeleteButton.textContent = "Delete";
+            newCheckbox.type = "checkbox";
+            newCell.appendChild(newCheckbox);
+            newCell.appendChild(newText);
+            newCell.appendChild(newDeleteButton);
+            todoTask.value = null;
+            newDeleteButton.addEventListener("click", function(event){
+            todoTable.deleteRow(event.target.parentElement.parentElement.rowIndex);
+            });
+        }
     });
 }
